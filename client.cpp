18fb7cafff87443e250b4d724d08057da13c4bd9
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-03-09 03:03:30 -0400

    Implementing initial connection setup

--- client.cpp
+++ client.cpp
@@ -1,14 +1,103 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
 #include <thread>
 #include <atomic>
 #include <chrono>
 
+#define MAXPENDING 5
+
 using namespace std;
+
+class Connection {
+	public:
+		Connection(string ip, int port) : ip{ip}, server_port{port} {
+			init_listen();
+			init_send();
+		}
+
+		~Connection() { // Close socket
+		 }
+
+		void init_listen() {
+		    /* Initialize listening socket */
+		    if ((l_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		    	// BETTER ERROR HANDLING 
+		        cerr << "socket() failed";
+		      
+		    /* Build listening address structure */
+		    l_addr = {};     	
+		    l_addr.sin_family = AF_INET;                
+		    l_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+		    l_addr.sin_port = 0; // Random port to be determined later
+
+		    /* Bind */
+		    if (bind(l_socket, (struct sockaddr *)&l_addr, sizeof(l_addr)) < 0)
+		    	// BETTER ERROR HANDLING 
+		        cerr << "bind() failed";
+
+		    /* Listen to incoming connections */
+		    if (listen(l_socket, MAXPENDING) < 0)
+		    	// BETTER ERROR HANDLING 
+		        cerr << "listen() failed";
+
+		    /* Get client port number */
+		    socklen_t len = sizeof(l_addr);
+		    if (getsockname(l_socket, (struct sockaddr *)&l_addr, &len) < 0)
+		    	cerr << "getsockname() failed";
+		    l_port = ntohs(l_addr.sin_port);
+		}
+
+		void init_send() {
+			/* Initialize connecting socket */
+			if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+				// BETTER ERROR HANDLING
+				cerr << "socket() failed";
+
+			// Better name later
+			struct hostent *s_host;
+			s_host = gethostbyname(ip.c_str());
+
+			/* Build server address structure */
+		    server_addr = {};     	
+		    server_addr.sin_family = AF_INET;               
+		    memcpy((char *) s_host->h_addr, // Copy given server hostname to server address
+		    	   (char *)&server_addr.sin_addr.s_addr, s_host->h_length);
+		    server_addr.sin_port = htons(server_port); 
+		}
+
+		// void receive();
+		void send(string message) {
+			/* Connect to server */
+			if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+				// BETTER ERROR HANDLING
+				cerr << "connect() failed";
+
+			if (write(client_socket, message.c_str(), message.length()) < 0)
+				//BETTER ERROR HANDLING
+				cerr << "write() failed";
+
+		}
+
+	private:
+		int l_socket;
+		int l_port;
+		struct sockaddr_in l_addr;
+		
+		string ip;
+		int server_port;
+		int client_socket;
+		struct sockaddr_in server_addr;
+
+};
 
 class Client {
 	public:
-		Client(string ip, int port) : ip{ip}, port{port}, m_stop{false} {
+		Client(string ip, int port) : m_stop{false}, connection{ip, port} {
 			cout << "Created a client!\n";
 			// synchronously identify here
 		}
@@ -26,14 +115,12 @@ class Client {
 		}
 
 	private:
-		string ip;
-		int port;
+		Connection connection;
 		thread input_thread;
 		// thread receive_thread;
 		atomic<bool> m_stop;
 
 		void init_receive() {
-
 		}
 
 		void init_input() {
